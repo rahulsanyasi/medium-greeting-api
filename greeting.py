837c3d2020e769a2a13d8d1ac26945ddaa3b1338
rahulsanyasi <rahulsanyasi@gmail.com>
2022-04-24 13:41:54 +0530

    edited welcome message to Welcome to Rahul's medium-greeting-api!

--- greeting.py
+++ greeting.py
@@ -45,7 +45,7 @@ def post_something():
 @app.route('/')
 def index():
     # A welcome message to test our server
-    return "<h1>Welcome to our medium-greeting-api!</h1>"
+    return "<h1>Welcome to Rahul's medium-greeting-api!</h1>"
 
 
 if __name__ == '__main__':
